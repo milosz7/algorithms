dc31e99541940df09a02d1300428f5eaac99573b
Milosz B <miloszbargiel@gmail.com>
2023-10-12 15:49:26 +0200

    Add ArrayStack implementation

---
+++ Zestaw01/ArrayStack.cpp
@@ -0,0 +1,20 @@
+#include <stdexcept>
+#include "ArrayStack.hpp"
+
+bool ArrayStack::empty() { return current == 0; }
+
+void ArrayStack::push(int x) {
+    if (current == _capacity)
+        throw std::out_of_range("Stack size exceeded.");
+    
+    stack[++current] = x;
+}
+
+int ArrayStack::pop() {
+    if (empty())
+        throw std::out_of_range("Stack is empty.");
+
+    return stack[current--];
+}
+
+int ArrayStack::size() { return current; }

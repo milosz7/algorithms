d198414f3db731f563945a04ebea9727f7bae106
Milosz B <miloszbargiel@gmail.com>
2023-11-06 10:52:46 +0100

    LinkedList.cpp code cleanup

--- Zestaw03/LinkedList.cpp
+++ Zestaw03/LinkedList.cpp
@@ -1,7 +1,5 @@
 #include "LinkedList.hpp"
 #include <iostream>
-#include <vector>
-#include <sstream>
 
 #define PUSH_FRONT 'F'
 #define PUSH_BACK 'B'
@@ -13,38 +11,25 @@
 #define REPLACE_SUCCESS "TRUE"
 #define REPLACE_FAILED "FALSE"
 
-void split_string(std::vector<std::string>&v, std::string &str) {
-    std::stringstream ss(str);
-    std::string substr;
-    while (ss >> substr) {
-        std::cout << substr;
-        v.push_back(substr);
-    }
-}
-
 int main()
 {
     int n_operations;
+    char operation;
     std::cin >> n_operations;
-    std::cin.ignore();
-    std::string line;
 
     LinkedList<int> list = LinkedList<int>();
-    std::vector<std::string> line_split;
 
     for (int i = 0; i < n_operations; i++) {
-        line_split.clear();
-        std::getline(std::cin, line);
-        
-        split_string(line_split, line);
-        char operation = line_split[0][0];
+        std::cin >> operation;
 
         if (operation == PUSH_FRONT) {
-            int to_push = std::stoi(line_split[1]);
+            int to_push;
+            std::cin >> to_push;
             list.push_front(to_push);
         }
         if (operation == PUSH_BACK) {
-            int to_push = std::stoi(line_split[1]);
+            int to_push;
+            std::cin >> to_push;
             list.push_back(to_push);
         }
         if (operation == POP_FIRST) {
@@ -66,21 +51,20 @@ int main()
             }
         }
         if (operation == REPLACE) {
-            int oldv = std::stoi(line_split[1]);
-            int newv = std::stoi(line_split[2]);
+            int oldv;
+            int newv;
+            std::cin >> oldv >> newv;
             int idx = list.find(oldv);
-            std::cout << oldv + newv;
-            // return 0;
             if (idx == -1) {
                 std::cout << REPLACE_FAILED << std::endl;
             } else {
                 list.insert(idx, newv);
-                list.erase(idx);
+                list.erase(idx + 1);
                 std::cout << REPLACE_SUCCESS << std::endl;
             }
         }
         if (operation == SIZE)
-            std::cout << list.size() << std::endl; 
+            std::cout << list.size() << std::endl;
     }
     return 0;
 }

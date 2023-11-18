0fb4a4781f50ed8c05c9563e6111774ba3a4c7e9
Milosz B <miloszbargiel@gmail.com>
2023-11-18 15:45:26 +0100

    Add BubbleSort implementation

---
+++ Zestaw05/BubbleSort.hpp
@@ -0,0 +1,21 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+#include <vector>
+
+template <typename T>
+void sort(std::vector<T> &v) {
+    bool swapped;
+    int n = v.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (v.at(j) > v.at(j+1)) {
+                swapped = true;
+                std::swap(v.at(j), v.at(j+1));
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+#endif

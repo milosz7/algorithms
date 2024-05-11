134224de76291a9bc6cc9bd1ac5eaa200d7087d5
Miłosz Bargieł <miloszbargiel@gmail.com>
2024-05-11 16:32:36 +0200

    Add tests for TopologicalSort algorithm

---
+++ topological_sort/TopologicalSortTest.py
@@ -0,0 +1,24 @@
+import unittest
+from topological_sort.TopologicalSort import TopologicalSort
+from graph.MatrixGraph import MatrixGraph
+
+
+class TestTopologicalSort(unittest.TestCase):
+    def setUp(self):
+        self.graph = MatrixGraph(7)
+        self.graph.add_edge(0, 1)
+        self.graph.add_edge(1, 2)
+        self.graph.add_edge(2, 3)
+        self.graph.add_edge(3, 4)
+        self.topological_sort = TopologicalSort(graph_object=self.graph)
+
+    def test_sort(self):
+        self.assertListEqual(self.topological_sort.sort(), [0, 1, 2, 3, 4])
+
+    def test_sort_with_cycle(self):
+        self.graph.add_edge(4, 0)
+        self.assertEqual(self.topological_sort.sort(), "ERROR - cycle detected")
+
+
+if __name__ == '__main__':
+    unittest.main()
